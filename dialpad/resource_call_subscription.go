5bfe00296542a2176ea605b87cc577ce1a27eb6c
Tom Rudick <tmr@storyhealth.ai>
2022-07-06 12:02:21 -0400

    Initial implementation of a Dialpad provider

---
+++ dialpad/resource_call_subscription.go
@@ -0,0 +1,173 @@
+package dialpad
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+type callSubscriptionRequest struct {
+	CallStates []string `json:"call_states"`
+	EndpointId string   `json:"endpoint_id"`
+}
+
+type callSubscriptionResponse struct {
+	Id         string          `json:"id"`
+	CallStates []string        `json:"call_states"`
+	Webhook    WebhookResponse `json:"webhook"`
+}
+
+func resourceCallSubscription() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceCallSubscriptionCreate,
+		ReadContext:   resourceCallSubscriptionRead,
+		UpdateContext: resourceCallSubscriptionUpdate,
+		DeleteContext: resourceCallSubscriptionDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: map[string]*schema.Schema{
+			"call_states": {
+				Type:     schema.TypeSet,
+				Required: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"endpoint_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceCallSubscriptionRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	subId := d.Id()
+	client := m.(*Client)
+
+	req, err := client.NewRequest("GET", fmt.Sprintf("/subscriptions/call/%s", subId), nil)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	sub := callSubscriptionResponse{}
+	err = json.Unmarshal(res, &sub)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.Set("call_states", sub.CallStates)
+	d.Set("endpoint_id", sub.Webhook.Id)
+
+	return diags
+}
+
+func resourceCallSubscriptionCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*Client)
+
+	statesSet := d.Get("call_states").(*schema.Set).List()
+	states := make([]string, len(statesSet))
+	for i, raw := range statesSet {
+		states[i] = raw.(string)
+	}
+
+	subRequest := callSubscriptionRequest{
+		CallStates: states,
+		EndpointId: d.Get("endpoint_id").(string),
+	}
+
+	body, err := json.Marshal(subRequest)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	req, err := client.NewRequest("POST", "/subscriptions/call", bytes.NewBuffer(body))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	sub := callSubscriptionResponse{}
+	err = json.Unmarshal(res, &sub)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(sub.Id)
+
+	return resourceCallSubscriptionRead(ctx, d, m)
+}
+
+func resourceCallSubscriptionUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*Client)
+	id := d.Id()
+
+	statesSet := d.Get("call_states").(*schema.Set).List()
+	states := make([]string, len(statesSet))
+	for i, raw := range statesSet {
+		states[i] = raw.(string)
+	}
+
+	subRequest := callSubscriptionRequest{
+		CallStates: states,
+		EndpointId: d.Get("endpoint_id").(string),
+	}
+
+	body, err := json.Marshal(subRequest)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	req, err := client.NewRequest("PATCH", fmt.Sprintf("/subscriptions/call/%s", id), bytes.NewBuffer(body))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	_, err = client.Do(req)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return resourceCallSubscriptionRead(ctx, d, m)
+}
+
+func resourceCallSubscriptionDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	client := m.(*Client)
+
+	id := d.Id()
+
+	req, err := client.NewRequest("DELETE", fmt.Sprintf("/subscriptions/call/%s", id), nil)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	_, err = client.Do(req)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId("")
+
+	return diags
+}
---
+++ dialpad/resource_webhook.go
@@ -0,0 +1,175 @@
+package dialpad
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+type signature struct {
+	Algorithm string `json:"algo"`
+	Secret    string `json:"secret"`
+	Type      string `json:"type"`
+}
+
+type webhookRequest struct {
+	HookUrl string `json:"hook_url"`
+	Secret  string `json:"secret"`
+}
+
+type WebhookResponse struct {
+	Id        string    `json:"id"`
+	HookUrl   string    `json:"hook_url"`
+	Signature signature `json:"signature"`
+}
+
+func resourceWebhook() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceWebhookCreate,
+		ReadContext:   resourceWebhookRead,
+		UpdateContext: resourceWebhookUpdate,
+		DeleteContext: resourceWebhookDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: map[string]*schema.Schema{
+			"hook_url": {
+				Type:     schema.TypeString,
+				Required: true,
+				Computed: false,
+			},
+			"secret": {
+				Type:      schema.TypeString,
+				Optional:  true,
+				Sensitive: true,
+				Computed:  false,
+			},
+		},
+	}
+}
+
+func resourceWebhookRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+
+	hookId := d.Id()
+	client := m.(*Client)
+
+	req, err := client.NewRequest("GET", fmt.Sprintf("/webhooks/%s", hookId), nil)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	hook := WebhookResponse{}
+	err = json.Unmarshal(res, &hook)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.Set("hook_url", hook.HookUrl)
+	d.Set("secret", hook.Signature.Secret)
+
+	return diags
+}
+
+func resourceWebhookCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*Client)
+
+	hookRequest := webhookRequest{
+		HookUrl: d.Get("hook_url").(string),
+	}
+
+	secret := d.Get("secret")
+	if secret != "" {
+		hookRequest.Secret = secret.(string)
+	}
+
+	body, err := json.Marshal(hookRequest)
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	req, err := client.NewRequest("POST", "/webhooks", bytes.NewBuffer(body))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	hook := WebhookResponse{}
+	err = json.Unmarshal(res, &hook)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(hook.Id)
+
+	return resourceWebhookRead(ctx, d, m)
+}
+
+func resourceWebhookUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	client := m.(*Client)
+	id := d.Id()
+
+	hookRequest := webhookRequest{
+		HookUrl: d.Get("hook_url").(string),
+	}
+
+	secret := d.Get("secret")
+	if secret != "" {
+		hookRequest.Secret = secret.(string)
+	}
+
+	body, err := json.Marshal(hookRequest)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	req, err := client.NewRequest("PATCH", fmt.Sprintf("/webhooks/%s", id), bytes.NewBuffer(body))
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	_, err = client.Do(req)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return resourceWebhookRead(ctx, d, m)
+}
+
+func resourceWebhookDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	client := m.(*Client)
+
+	id := d.Id()
+
+	req, err := client.NewRequest("DELETE", fmt.Sprintf("/webhooks/%s", id), nil)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	_, err = client.Do(req)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId("")
+
+	return diags
+}
